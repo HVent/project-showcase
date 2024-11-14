3c69dfe5ffc6f7a8787b6d8602be245f0984e951
Heinrich Vent <92303848+HVent@users.noreply.github.com>
2024-11-14 07:53:23 +0100

    Add files via upload

---
+++ project-alfatraining-java-course/HvZwischenpruefung/src/documentation/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"Alle Packages","u":"allpackages-index.html"},{"l":"commands"},{"l":"logic.factories"},{"l":"logic.filehandling"},{"l":"logic.manager"},{"l":"main"},{"l":"model"},{"l":"test"},{"l":"texts"},{"l":"ui"}];updateSearchResults();
